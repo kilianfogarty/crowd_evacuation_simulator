902531fd0592acd51daafa287e948acf87cb3a08
Kilian Fogarty <kilian.fogarty@icloud.com>
2026-03-10 18:19:35 -0400

    Added the  simulation and visualization classes

---
+++ src/crowd_evacuation_simulator/simulation.py
@@ -0,0 +1,27 @@
+from src.crowd_evacuation_simulator.environment import Environment
+from src.crowd_evacuation_simulator.exit import Exit
+
+
+class Simulation:
+    def __init__(self, environment: Environment, dt: float = 0.1) -> None:
+        self.environment: Environment = environment
+        self.dt: float = dt
+        self.time: float = 0
+
+    def step(self) -> None:
+        for agent in self.environment.agents:
+
+            if agent.evacuated:
+                continue
+
+            exit: Exit = self.environment.exits[0]
+
+            agent.step_toward(exit.position, self.dt)
+
+            if exit.check_if_agent_at_exit(agent):
+                agent.evacuated = True
+
+        self.time += self.dt
+        
+
+
---
+++ src/crowd_evacuation_simulator/visualization.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+from src.crowd_evacuation_simulator.environment import Environment
+
+class Visualizer:
+    def __init__(self, environment: Environment) -> None:
+        self.environment: Environment = environment
+    
+    def plot(self) -> None:
+        plt.clf() #Clears anything already present
+
+        for agent in self.environment.agents:
+            if not agent.evacuated:
+                plt.scatter(agent.position[0], agent.position[1])
+        
+        for exit in self.environment.exits:
+            plt.scatter(
+                exit.position[0],
+                exit.position[1],
+                marker="s",
+                s=100
+            )
+
+        plt.xlim(0, self.environment.width)
+        plt.xlim(0, self.environment.height)
+
+        plt.pause(0.01)
+
