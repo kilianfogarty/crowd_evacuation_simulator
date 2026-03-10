26e884af8c59b0a679e38d9d1e7163ecd1323f50
Kilian Fogarty <kilian.fogarty@icloud.com>
2026-03-10 15:58:31 -0400

    Implemented a simple circular exit for now

---
+++ src/crowd_evacuation_simulator/exit.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Exit:
+    def __init__(self, position: np.ndarray | list[float], radius: float) -> None:
+        self.position: np.ndarray = np.array(position, dtype=float)
+        self.radius: float = radius
+
+        def check_if_agent_at_exit(self, agent: Agent) -> bool:
+            distance: float = np.linalg.norm(agent.position - self.position)
+            return distance < self.radius
