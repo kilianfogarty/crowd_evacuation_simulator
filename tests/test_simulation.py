d3e82ff0de01dc129dee76d2404179ae23f3a314
Kilian Fogarty <kilian.fogarty@icloud.com>
2026-03-10 22:09:01 -0400

    Added more test cases for the simulation class

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -13,3 +13,64 @@ class TestSimulation:
         assert simulation.environment is environment
         assert np.allclose(simulation.dt, 0.5)
         assert np.allclose(simulation.time, 0)
+
+    def test_step_moves_agent_toward_exit(self) -> None:
+        environment: Environment = Environment(10,10)
+        agent: Agent = Agent([3,3], speed=1.0)
+        exit: Exit = Exit([0,0], radius=0.5)
+
+        environment.add_agent(agent)
+        environment.add_exit(exit)
+        simulation: Simulation = Simulation(environment, dt=1.0)
+
+        simulation.step()
+
+        distance_before_step = np.linalg.norm(np.array([3,3]) - exit.position)
+        deistance_after_step = np.linalg.norm(agent.position - exit.position)
+
+        assert distance_before_step > deistance_after_step
+
+    def test_agent_evacuates_upon_reaching_exit(self):
+        environment: Environment = Environment(10,10)
+        agent: Agent = Agent([0,0], speed=1.0)
+        exit: Exit = Exit([0,0], radius=0.5)
+
+        environment.add_agent(agent)
+        environment.add_exit(exit)
+        simulation: Simulation = Simulation(environment, dt=1.0)
+
+        simulation.step()  
+
+        assert agent.evacuated is True
+
+    def test_multiple_agents_evacuate(self):
+        environment: Environment = Environment(10,10)
+        exit: Exit = Exit([0,0], radius=0.5)
+
+        agents: list[Agent] = [Agent([3,4]), Agent([3,5]), Agent([2,4]), Agent([4,4]), Agent([3,3])]
+        for agent in agents:
+            environment.add_agent(agent)
+        environment.add_exit(exit)
+        
+        simulation: Simulation = Simulation(environment, dt=1.0)
+
+        while not all(agent.evacuated for agent in agents):
+            simulation.step()
+
+        for agent in agents:
+            assert agent.evacuated is True
+
+    def test_time_updates_after_step(self):
+        environment: Environment = Environment(10,10)
+        agent: Agent = Agent([0,0], speed=1.0)
+        exit: Exit = Exit([0,0], radius=0.5)
+
+        environment.add_agent(agent)
+        environment.add_exit(exit)
+        simulation: Simulation = Simulation(environment, dt=1.0)
+        initial_time = simulation.time
+
+        simulation.step()
+
+        assert np.allclose((initial_time + simulation.dt), simulation.time)
+
