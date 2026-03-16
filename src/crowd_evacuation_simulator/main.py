053f88b415bda7149bbf9164b49ec55eaea050ec
Kilian Fogarty <kilian.fogarty@icloud.com>
2026-03-16 14:28:19 -0800

    Added a main runner

---
+++ src/crowd_evacuation_simulator/main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+from .environment import Environment
+from .agent import Agent
+from .exit import Exit
+from .simulation import Simulation
+from .visualization import Visualization
+
+environment: Environment = Environment(width=20, height=20)
+environment.add_exit(Exit[18,10], radius=1)
+
+for i in range(30):
+    position: list[float] = [random.random() * environment.width, random.random() * environment.width]
+    environment.add_agent(Agent(position))
+
+simulation: Simulation = Simulation(environment)
+visualization: Visualization = Visualization(environment)
+
+plt.ion() #Interactivity
+
+for i in range(400):
+    simulation.step()
+    visualization.plot()
+
